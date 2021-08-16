39a9d82efbdd670aa505bafce460dd52681b2167
SongZhiyuan <songzhiyuan@sailvan.com>
2021-08-16 05:43:21 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,7 +20,7 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if(Auth::attempt($creditials))
+        if(Auth::attempt($creditials,$request->has('remember')))
         {
             //登录成功
             session()->flash('success','欢迎回来！');
@@ -38,7 +38,7 @@ class SessionsController extends Controller
     public function destroy()
     {
         Auth::logout();
-        session()->flash('success'.'您已成功推出！');
+        session()->flash('success'.'您已成功退出！');
         return redirect('login');
     }
 }
