b3c870fed45721fddf1dc50f3e1534539100d137
SongZhiyuan <songzhiyuan@sailvan.com>
2021-08-25 05:57:44 +0000

    关注按钮

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -25,4 +25,9 @@ class UserPolicy
         //当前用户是管理员且删除的不是自己
         return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
+
+    public function follow(User $currentUser,User $user)
+    {
+        return $currentUser->id !== $user->id;
+    }
 }
