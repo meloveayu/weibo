217e3bc1f95c655213974bdc6d490ff33f607932
meloveayu <meloveayu@sina.com>
2023-09-07 06:25:15 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,9 +17,14 @@ class UsersController extends Controller
 
     }
 
-    public function store()
+    public function store(Request $request)
     {
-
+        //这个地方主要是为了字段的验证规则，这个步骤是在储存前做的
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
     }
 
     public function show(User $user)
